4cf9e396a2cd7093e3211ad520ccfca20d6c4f5c
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2025-03-08 14:04:17 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -805,7 +805,6 @@ if st.session_state.reset:
     with st.empty():
         while time.time() <= end_time:
             with st.container():
-                post_time_dict
                 time_now = datetime.now() + datere.relativedelta(hours=8)
                 odds = get_odds_data()
                 investments = get_investment_data()
