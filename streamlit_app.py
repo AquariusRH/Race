acf6fa857f340c45cc9d920be9fce688340ae4de
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2025-06-14 16:21:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import altair as alt
+import altair as altMore actions
 import pandas as pd
 import streamlit as st
 import requests
@@ -288,109 +288,107 @@ def print_bar_chart(time_now):
   time_5_minutes_before = np.datetime64(post_time - timedelta(minutes=5) + timedelta(hours=8))
 
   for method in print_list:
-      fig, ax1 = plt.subplots(figsize=(12, 6))
-      odds_list = pd.DataFrame()
-      if method == 'overall':
+    fig, ax1 = plt.subplots(figsize=(12, 6))
+    odds_list = pd.DataFrame()
+    if method == 'overall':
           df = overall_investment_dict[method]
           change_data = diff_dict[method].iloc[-1]
-      elif method == 'qin_qpl':
+    elif method == 'qin_qpl':
           df = overall_investment_dict['QIN'] + overall_investment_dict['QPL']
           change_data = diff_dict['QIN'].tail(20).sum(axis = 0) + diff_dict['QPL'].tail(20).sum(axis = 0)
-      elif method == 'qin':
+    elif method == 'qin':
           df = overall_investment_dict['QIN']
           change_data = diff_dict[method].tail(20).sum(axis = 0)
-      elif method in ['WIN', 'PLA']:
+    elif method in ['WIN', 'PLA']:
           df = overall_investment_dict[method]
           odds_list = odds_dict[method]
           change_data = diff_dict[method].tail(20).sum(axis = 0)
 
-      df.index = pd.to_datetime(df.index)
-      df_1st = pd.DataFrame()
-      df_2nd = pd.DataFrame()
-      #df_3rd = pd.DataFrame()
+    df.index = pd.to_datetime(df.index)
+    df_1st = pd.DataFrame()
+    df_2nd = pd.DataFrame()
+    df_3rd = pd.DataFrame()
 
-      df_1st = df[df.index< time_25_minutes_before].tail(1)
+    df_1st = df[df.index< time_25_minutes_before].tail(1)
 
-      df_2nd = df[df.index >= time_25_minutes_before].tail(1)
+    df_2nd = df[(df.index >= time_25_minutes_before) & (df.index < time_5_minutes_before)].tail(1)
 
-      #df_3rd = df[df.index>= time_5_minutes_before].tail(1)
+    df_3rd = df[df.index>= time_5_minutes_before].tail(1)
 
-      change_df = pd.DataFrame([change_data.apply(lambda x: x*3 if x > 0 else x)],columns=change_data.index,index =[df.index[-1]])
-      print(change_df)
-      if method in ['WIN', 'PLA']:
+    change_df = pd.DataFrame([change_data.apply(lambda x: x*3 if x > 0 else x)],columns=change_data.index,index =[df.index[-1]])
+    if method in ['WIN', 'PLA']:
         odds_list.index = pd.to_datetime(odds_list.index)
         odds_1st = odds_list[odds_list.index< time_25_minutes_before].tail(1)
-        odds_2nd = odds_list[odds_list.index >= time_25_minutes_before].tail(1)
-        #odds_3rd = odds_list[odds_list.index>= time_5_minutes_before].tail(1)
+        odds_2nd = odds_list[(odds_list.index >= time_25_minutes_before) & (odds_list.index < time_5_minutes_before)].tail(1)
+        odds_3rd = odds_list[odds_list.index>= time_5_minutes_before].tail(1)
 
-      bars_1st = None
-      bars_2nd = None
-      #bars_3rd = None
-      data_df = df_1st._append(df_2nd)
-      #final_data_df = data_df._append(df_3rd)
-      final_data_df = data_df
-      sorted_final_data_df = final_data_df.sort_values(by=final_data_df.index[0], axis=1, ascending=False)
-      diff = sorted_final_data_df.diff().dropna()
-      diff[diff < 0] = 0
-      X = sorted_final_data_df.columns
-      X_axis = np.arange(len(X))
-      sorted_change_df = change_df[X]
-      if not df_1st.empty:
-          if df_2nd.empty:
+    bars_1st = None
+    bars_2nd = None
+    bars_3rd = None
+    data_df = df_1st._append(df_2nd)
+    final_data_df = data_df._append(df_3rd)
+    sorted_final_data_df = final_data_df.sort_values(by=final_data_df.index[0], axis=1, ascending=False)
+    diff = sorted_final_data_df.diff().dropna()
+    diff[diff < 0] = 0
+    X = sorted_final_data_df.columns
+    X_axis = np.arange(len(X))
+    sorted_change_df = change_df[X]
+    if not df_1st.empty:
+        if df_2nd.empty:
               bars_1st = ax1.bar(X_axis, sorted_final_data_df.iloc[0], 0.4, label='投注額', color='pink')
-          else:
-              bars_2nd = ax1.bar(X_axis - 0.2, sorted_final_data_df.iloc[1], 0.4, label='25分鐘', color='blue')
-              bar = ax1.bar(X_axis+0.2,sorted_change_df.iloc[0],0.4,label='改變',color='grey')
-              #if not df_3rd.empty:
-                  #bars_3rd = ax1.bar(X_axis, diff.iloc[0], 0.3, label='5分鐘', color='red')
-      else:
+        else:
+              bars_2nd = ax1.bar(X_axis - 0.3, sorted_final_data_df.iloc[1], 0.3, label='25分鐘', color='blue')
+              bar = ax1.bar(X_axis+0.3,sorted_change_df.iloc[0],0.3,label='改變',color='grey')
+              if not df_3rd.empty:
+                  bars_3rd = ax1.bar(X_axis, diff.iloc[0], 0.3, label='5分鐘', color='red')
+    else:
           if not df_2nd.empty:
-              bars_2nd = ax1.bar(X_axis - 0.2, sorted_final_data_df.iloc[0], 0.4, label='25分鐘', color='blue')
+              bars_2nd = ax1.bar(X_axis - 0.3, sorted_final_data_df.iloc[0], 0.3, label='25分鐘', color='blue')
+              bar = ax1.bar(X_axis+0.3,sorted_change_df.iloc[0],0.3,label='改變',color='grey')
+              if not df_3rd.empty:
+                  bars_3rd = ax1.bar(X_axis, diff.iloc[0], 0.3, label='5分鐘', color='red')
+          else:
+              bars_3rd = ax1.bar(X_axis-0.2, sorted_final_data_df.iloc[0], 0.4, label='5分鐘', color='red')
               bar = ax1.bar(X_axis+0.2,sorted_change_df.iloc[0],0.4,label='改變',color='grey')
-              #if not df_3rd.empty:
-                  #bars_3rd = ax1.bar(X_axis, diff.iloc[0], 0.3, label='5分鐘', color='red')
-          #else:
-              #bars_3rd = ax1.bar(X_axis-0.2, sorted_final_data_df.iloc[0], 0.4, label='5分鐘', color='red')
-              #bar = ax1.bar(X_axis+0.2,sorted_change_df.iloc[0],0.4,label='改變',color='grey')
 
       # Add numbers above bars
-      if method in ['WIN', 'PLA']:
+    if method in ['WIN', 'PLA']:
         if bars_2nd is not None:
           sorted_odds_list_2nd = odds_2nd[X].iloc[0]
           for bar, odds in zip(bars_2nd, sorted_odds_list_2nd):
               yval = bar.get_height()
               ax1.text(bar.get_x() + bar.get_width() / 2, yval, odds, ha='center', va='bottom')
-        #if bars_3rd is not None:
-          #sorted_odds_list_3rd = odds_3rd[X].iloc[0]
-          #for bar, odds in zip(bars_3rd, sorted_odds_list_3rd):
-               # yval = bar.get_height()
-                #ax1.text(bar.get_x() + bar.get_width() / 2, yval, odds, ha='center', va='bottom')
+        if bars_3rd is not None:
+          sorted_odds_list_3rd = odds_3rd[X].iloc[0]
+          for bar, odds in zip(bars_3rd, sorted_odds_list_3rd):
+                yval = bar.get_height()
+                ax1.text(bar.get_x() + bar.get_width() / 2, yval, odds, ha='center', va='bottom')
         elif bars_1st is not None:
           sorted_odds_list_1st = odds_1st[X].iloc[0]
           for bar, odds in zip(bars_1st, sorted_odds_list_1st):
               yval = bar.get_height()
               ax1.text(bar.get_x() + bar.get_width() / 2, yval, odds, ha='center', va='bottom')
 
-      namelist_sort = [numbered_dict[race_no][i - 1] for i in X]
-      formatted_namelist = [label.split('.')[0] + '.' + '\n'.join(label.split('.')[1]) for label in namelist_sort]
-      plt.xticks(X_axis, formatted_namelist, fontsize=12)
-      ax1.grid(color='lightgrey', axis='y', linestyle='--')
-      ax1.set_ylabel('投注額',fontsize=15)
-      ax1.tick_params(axis='y')
-      fig.legend()
+    namelist_sort = [numbered_dict[race_no][i - 1] for i in X]
+    formatted_namelist = [label.split('.')[0] + '.' + '\n'.join(label.split('.')[1]) for label in namelist_sort]
+    plt.xticks(X_axis, formatted_namelist, fontsize=15)
+    ax1.grid(color='lightgrey', axis='y', linestyle='--')
+    ax1.set_ylabel('投注額',fontsize=15)
+    ax1.tick_params(axis='y')
+    fig.legend()
 
-      if method == 'overall':
+    if method == 'overall':
           plt.title('綜合', fontsize=15)
-      elif method == 'qin_qpl':
+    elif method == 'qin_qpl':
           plt.title('連贏 / 位置Q', fontsize=15)
-      elif method == 'qin':
+    elif method == 'qin':
           plt.title('連贏', fontsize=15)
-      elif method == 'WIN':
+    elif method == 'WIN':
           plt.title('獨贏', fontsize=15)
-      elif method == 'PLA':
+    elif method == 'PLA':
           plt.title('位置', fontsize=15)
-      st.pyplot(fig)
-      
+
+    st.pyplot(fig)
 
 def weird_data(investments):
   for method in methodlist:
@@ -418,7 +416,7 @@ def weird_data(investments):
           highlight = '***'
         error_df = pd.DataFrame([[index,error,odds_dict[method].tail(1)[index].values,highlight]], columns=['No.', 'error','odds', 'Highlight'],index = diff.index)
       weird_dict[method] = weird_dict[method]._append(error_df)
-      
+
 def change_overall(time_now):
   total_investment = diff_dict['WIN'].sum(axis=0)+diff_dict['PLA'].sum(axis=0)+diff_dict['QIN'].sum(axis=0)+diff_dict['QPL'].sum(axis=0)
   total_investment_df = pd.DataFrame([total_investment],index = [time_now])
@@ -432,14 +430,14 @@ def print_concern_weird_dict():
       df_tail = df.tail(20)[::-1]
       count = df.value_counts('No.')
       count_df = count.to_frame().T
-  
+
       # Create two columns
       col1, col2 = st.columns(2)
-  
+
       # Display df_tail in the first column
       with col1:
          df_tail
-  
+
       # Display count_df in the second column
       with col2:
           count_df
@@ -526,7 +524,7 @@ def top(method_odds_df, method_investment_df, method):
     second_last_row_investment_df['Previous_Investment_Change'] = second_last_row_investment_df['Previous_Investment_Change'].apply(lambda x: x if x > 0 else 0)
     third_last_row_investment_df['Previous_Investment_Change'] = last_row_investment_df['Investment'] - third_last_row_investment_df['Investment'].values
     third_last_row_investment_df['Previous_Investment_Change'] = third_last_row_investment_df['Previous_Investment_Change'].apply(lambda x: x if x > 0 else 0)
-    
+
     # Sort the final DataFrame by odds value
     final_df = final_df.sort_values(by='Odds')
 
@@ -555,8 +553,8 @@ def top(method_odds_df, method_investment_df, method):
       # Display the styled DataFrame
       st.write(styled_df.to_html(), unsafe_allow_html=True)
       st.write(styled_rows_with_plus.to_html(), unsafe_allow_html=True)
-  
-  
+
+
     else:
       final_df.columns = ['組合', '賠率', '最初賠率', '排名', '最初排名', '上一次排名', '投注變化', '投注', '一分鐘投注','五分鐘投注']
       target_df = final_df.head(25)
@@ -564,8 +562,8 @@ def top(method_odds_df, method_investment_df, method):
           target_df['最初排名'].astype(str).str.contains('\+') |
           target_df['上一次排名'].astype(str).str.contains('\+')
       ][['組合', '賠率', '最初排名', '上一次排名']]
-  
-  
+
+
       # Apply the conditional formatting to the 初始排名 and 前一排名 columns and add a bar to the 投資變化 column
       styled_df = target_df.style.format({
         '賠率': '{:.1f}',
@@ -882,10 +880,10 @@ if not st.session_state.api_called:
       }
       """
   }
-  
+
   # Make a POST request to the API
   response = requests.post(url, json=payload)
-  
+
   # Check if the request was successful
   if response.status_code == 200:
       data = response.json()
@@ -914,10 +912,10 @@ if not st.session_state.api_called:
                   race_dict[race_number]["練馬師"].append(trainer_name_ch)
                   race_dict[race_number]["最近賽績"].append(last6run)
       print('完成')
-  
+
   else:
       print(f'Failed to retrieve data. Status code: {response.status_code}')
-  
+
   race_dataframes = {}
   numbered_dict ={}
   for race_number in race_dict:
