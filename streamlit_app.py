4009ccd80c4a83f5b93b9c81eae027ab3dd4ba79
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2025-06-27 23:44:20 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -581,11 +581,8 @@ def top(method_odds_df, method_investment_df, method):
 
     else:
       final_df.columns = ['組合', '賠率', '最初賠率', '排名', '最初排名', '上一次排名', '投注變化', '投注', '一分鐘投注','五分鐘投注']
-      if method in ["QIN","FCT"]:
-        target_df = final_df.head(15)
-        target_special_df = final_df.head(25)
-      else:
-        target_df = final_df.head(15)
+      target_df = final_df.head(15)
+      target_special_df = final_df.head(30)
       rows_with_plus = target_special_df[
           target_special_df['最初排名'].astype(str).str.contains('\+') |
           target_special_df['上一次排名'].astype(str).str.contains('\+')
