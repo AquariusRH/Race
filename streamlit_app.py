9aa11c343a6157200eb041ea8b14dfed4efac874
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2025-09-06 22:24:52 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -315,7 +315,7 @@ def print_bar_chart(time_now):
           df = overall_investment_dict[method]
           change_data = diff_dict[method].tail(10).sum(axis = 0)
           odds_list = odds_dict[method].tail(1)
-      st.write(df.tail(1).sum)
+      st.write(df.tail(1).sum(axis=1))
       fig, ax1 = plt.subplots(figsize=(12, 6))
       df.index = pd.to_datetime(df.index)
       df_1st = pd.DataFrame()
