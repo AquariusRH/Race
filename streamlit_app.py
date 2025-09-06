92d8f941ff14420657fdf58a5c27a35bf7fe8cb2
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2025-09-06 23:57:03 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -314,7 +314,7 @@ def print_bar_chart(time_now):
       elif method in methodlist:
           df = overall_investment_dict[method]
           change_data = diff_dict[method].tail(10).sum(axis = 0)
-          odds_list = odds_dict[method].tail(1)
+          odds_list = odds_dict[method]
       if df.tail(1).sum(axis=1)[0]==0:
         continue
       fig, ax1 = plt.subplots(figsize=(12, 6))
