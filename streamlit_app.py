c21d6f6091c0a190b63f0a1a483b985fd537d49d
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2025-09-28 23:41:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -697,8 +697,8 @@ with infoColumns[2]:
 # Initialize lists (using list2 and list2_ch as default; change to list1 and list1_ch if preferred)
 available_methods = ['WIN', 'PLA', 'QIN', 'QPL', 'FCT', 'TRI', 'FF']
 available_methods_ch = ['獨贏', '位置', '連贏', '位置Q', '二重彩', '單T', '四連環']
-print_list_default = ['PLA','QPL','QIN', 'WIN']
-default_checked_methods = ['WIN', 'PLA', 'QIN', 'QPL']
+print_list_default = ['QIN']
+default_checked_methods = ['QIN']
 # Initialize session state variables
 if 'reset' not in st.session_state:
     st.session_state.reset = False
